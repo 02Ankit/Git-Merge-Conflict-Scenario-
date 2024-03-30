99baf8a0e8723ac94282b4531845785f867d331c
nodoubtsacademy <02limzadewangan@gmail.com>
2024-03-30 22:52:09 +0530

    first commit add app.py and main.py

---
+++ src/app.py
@@ -0,0 +1,10 @@
+# create app.py
+def addition(a,b):
+    return a+b
+
+addition(4,5)
+
+def division(a,b):
+    return a/b
+
+division(4,5)
---
+++ src/main.py
@@ -0,0 +1,6 @@
+# create main.py
+class Hooli:
+    def __init__(self):
+        print("welcome to the python")
+
+python = Hooli()
